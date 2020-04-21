df3afa40593e94f612f4ac524600749ee556b674
Satyaki233 <satyakidesarkar2017@gmail.com>
2020-04-21 11:13:40 -0400

    fine

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import {BrowserRouter,Route,Switch} from 'react-router-dom'
 import './App.css';
 import About from './components/About';
 import Home from './components/Home'
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
      <BrowserRouter>
+       <Nav/>
        <Switch>
           <Route path='/' exact component={Home}/>
           <Route path='/About' exact component={About}/>
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,15 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            Home
+        <div className='jumbotron'>
+           <h1> Medica </h1> <br/>
+           <p>Online medical Shop for all your medicines</p>
+        </div>
+          <div>
+              <input type='text' style={{width:'50vw',height:'70px',fontSize:'3vw'}} placeholder="Type the name of medicine"/>
+          </div>
+         
+
         </div>
     )
 }
